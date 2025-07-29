7998488690f7c54cd5887d8ef160b9b6515a9f68
Luis Coelho <luis.miguel.coelho@schindler.com>
2025-07-29 11:02:08 +0000

    initial work

--- wemos_d1_mqtt/src/main.cpp
+++ wemos_d1_mqtt/src/main.cpp
@@ -1,52 +1,22 @@
 #include <Arduino.h>
+#include <SoftwareSerial.h>
+#include <TaskScheduler.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266Ping.h>
+#include <AsyncMqttClient.h>
 
 // WiFi and MQTT settings
 const char* ssid = "your_SSID";
 const char* password = "your_PASSWORD";
 const char* mqtt_server = "broker.hivemq.com";
-
-WiFiClient espClient;
-PubSubClient client(espClient);
+ 
 
 void setup() {
     Serial.begin(115200);
-    setup_wifi();
-    client.setServer(mqtt_server, 1883);
+   
 }
 
 void loop() {
-    if (!client.connected()) {
-        reconnect();
-    }
-    client.loop();
+    
 }
-
-void setup_wifi() {
-    delay(10);
-    Serial.println();
-    Serial.print("Connecting to ");
-    Serial.println(ssid);
-
-    WiFi.begin(ssid, password);
-    while (WiFi.status() != WL_CONNECTED) {
-        delay(500);
-        Serial.print(".");
-    }
-
-    Serial.println("");
-    Serial.println("WiFi connected");
-}
-
-void reconnect() {
-    while (!client.connected()) {
-        Serial.print("Attempting MQTT connection...");
-        if (client.connect("WemosD1Client")) {
-            Serial.println("connected");
-        } else {
-            Serial.print("failed, rc=");
-            Serial.print(client.state());
-            Serial.println(" try again in 5 seconds");
-            delay(5000);
-        }
-    }
-}
+ 
